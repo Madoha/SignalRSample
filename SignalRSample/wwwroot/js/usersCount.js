d76cab72ca78cf8c1f9143f5d1c1f0e5337c6cfc
Madoha <madoha00@gmail.com>
2024-04-04 18:08:18 +0600

    Добавьте файлы проекта.

---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,23 @@
+﻿var connectionUserCount = new signalR
+    .HubConnectionBuilder()
+    .withUrl("/hubs/userCount")
+    .build();
+
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+function fulfilled() {
+    console.log("success");
+    newWindowLoadedOnClient();
+}
+function rejected() {
+    console.log("failed");
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
